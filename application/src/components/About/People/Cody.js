688b1296bd5d40605f1e2886ce3b5cf980ac74c1
wenhuang415 <whuang17@mail.sfsu.edu>
2022-02-14 17:37:36 -0800

    finished cody about me

--- application/src/components/About/People/Cody.js
+++ application/src/components/About/People/Cody.js
@@ -1,5 +1,5 @@
 import React from "react";
-import pic from "../../../images/team.png";
+import pic from "../../../images/cody.png";
 import styles from "./index.module.css";
 import { Row, Col } from "react-bootstrap";
 
@@ -11,15 +11,18 @@ const Cody = () => {
           <Col style={{ textAlign: "center" }}>
             <div className={styles.centerDiv}>
               <Row>
-                <h3 className={styles.subtitle}>Hello, it's me</h3>
+                <h3 className={styles.subtitle}>Backend Lead</h3>
               </Row>
               {/* name Here */}
               <Row>
-                <h1 className={styles.title}>Name here.</h1>
+                <h1 className={styles.title}>Cody Huang</h1>
               </Row>
               {/* Fun Line here */}
               <Row>
-                <p className={styles.para}>Fun Line here</p>
+                <p className={styles.para}>I am the backend lead for Team 8. I enjoy coding specfically video games. 
+                They are my passion. I love mexican food. I am also an active person when I'm not on the computer. Some of the sports
+                I enjoy are river kayaking, wrestling, running, and snowboarding. I bring a lot of passion into
+                my projects.</p>
               </Row>
             </div>
           </Col>
