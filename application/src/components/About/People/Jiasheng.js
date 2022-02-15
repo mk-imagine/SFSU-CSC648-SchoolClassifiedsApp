c5b8f82093cc34bfb2fc2df05e765a502532b288
jiasheng-li <78129708+jiasheng-li@users.noreply.github.com>
2022-02-15 14:34:36 -0800

    Jiasheng Li About Page

--- application/src/components/About/People/Jiasheng.js
+++ application/src/components/About/People/Jiasheng.js
@@ -1,5 +1,5 @@
 import React from "react";
-import pic from "../../../images/team.png";
+import pic from "../../../images/JiashengLi.JPG";
 import styles from "./index.module.css";
 import { Row, Col } from "react-bootstrap";
 
@@ -15,11 +15,13 @@ const Jiasheng = () => {
               </Row>
               {/* name Here */}
               <Row>
-                <h1 className={styles.title}>Name here.</h1>
+                <h1 className={styles.title}>Jiasheng Li</h1>
               </Row>
               {/* Fun Line here */}
               <Row>
-                <p className={styles.para}>Fun Line here</p>
+                <p className={styles.para}>I am from Guangzhou, China which has lots of delicious local foods including dim sum,  
+                wonton noodles, and Char Siu. I like sports, especially basketball. 
+                My favorite basketball player is Kobe Bryant. Also, I am a sneakerhead who likes collecting sneakers. </p>
               </Row>
             </div>
           </Col>
