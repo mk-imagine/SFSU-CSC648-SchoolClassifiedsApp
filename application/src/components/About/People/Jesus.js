cae2476eb6fca608c5f0796efbc5f9274e5df0d5
Jesus Cervantes Fajardo <jcervantesfajardo@mail.sfsu.edu>
2022-02-15 19:12:14 -0800

    Added A picture to Jesus About Me page

--- application/src/components/About/People/Jesus.js
+++ application/src/components/About/People/Jesus.js
@@ -1,5 +1,5 @@
 import React from "react";
-import pic from "../../../images/team.png";
+import pic from "../../../images/Jesus.png";
 import styles from "./index.module.css";
 import { Row, Col } from "react-bootstrap";
 
@@ -11,15 +11,15 @@ const Jesus = () => {
           <Col style={{ textAlign: "center" }}>
             <div className={styles.centerDiv}>
               <Row>
-                <h3 className={styles.subtitle}>Hello, it's me</h3>
+                <h3 className={styles.subtitle}>Hello everyone!</h3>
               </Row>
               {/* name Here */}
               <Row>
-                <h1 className={styles.title}>Jesus Cervantes Fajardo.</h1>
+                <h1 className={styles.title}> I am Jesus Cervantes Fajardo.</h1>
               </Row>
               {/* Fun Line here */}
               <Row>
-                <p className={styles.para}>Hello everyone, I am a part of Team 8's back end developers! 
+                <p className={styles.para}>And I am a part of Team 8's back end developers! 
                 Some things I enjoy are video games, music, and fishing.</p>
               </Row>
             </div>
