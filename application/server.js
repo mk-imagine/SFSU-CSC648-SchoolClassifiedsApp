eb3bd12321e41ba186a93e6c83e2636989f822fd
wenhuang415 <whuang17@mail.sfsu.edu>
2022-02-17 19:07:21 -0800

    finished deploying express server

--- application/server.js
+++ application/server.js
@@ -1,7 +1,7 @@
 
 const express = require('express'); //Line 1
 const app = express(); //Line 2
-const port = process.env.PORT || 8080; //Line 3
+const port = process.env.PORT || 5000; //Line 3
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
