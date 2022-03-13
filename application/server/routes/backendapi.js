e0483a730a4477367c918129a0d047a55e3e9334
tdwlop <thisisvsemail@gmail.com>
2022-03-12 21:23:28 -0800

    added url

--- application/server/routes/backendapi.js
+++ application/server/routes/backendapi.js
@@ -1,84 +1,107 @@
-
-//___________________________________________--
+const aws = require('aws-sdk');
+const { createPool } = require('mysql2');
 //const db = require('../config/database');
-//I was going off a Youtube video
-const {createPool} = require('mysql2');
-                            
-//have to change
- const pool = createPool({       //have to change
-    host: 'localhost',
+
+/*___________________________________________*/
+
+//have to change information
+const pool = createPool({
+    host: 'mysql',
     user: 'root',
-    password: 'sfcs2020',
-    connectionLimit: 50
-}); 
+    password: 'csc648team8'
+});
+
+/*__________________________________________________________________________________________________*/
 
 //gets the array of categories
-//function getCategories(){
+function getCategories() {
     pool.query('select category_name from csc648.category', (err, res) => {
-    let arrayOfCategories = [];
-    res.map((data) => {        
-        let sData = Object.values(data);
-        arrayOfCategories.push(sData[0]);
-    })
-    console.log(arrayOfCategories);
-    return arrayOfCategories;
+        let arrayOfCategories = [];
+        res.map((data) => {
+            let sData = Object.values(data);
+            arrayOfCategories.push(sData[0]);
+        })
+        console.log(arrayOfCategories);
+        return arrayOfCategories;
     });
-//}
+}
 
-//gets the array of items
-/* pool.query('select * from csc648.item', (err, res) => {
-    let arrayOfItems = [];
-    res.map((data) => {
-        for(key in data){
-            if(key == "item_pic"){
-                let val = data[key];
-                //here run the s3 stuff to get the image url. find using the value and save result
+/*__________________________________________________________________________________________________*/
 
-
-                //then replace the val with the saved url result
-                let x = "temp_url"
-                data[key] = x;
-                //console.log(data);
+//gets the array of all items
+function getAllItems() {
+    pool.query('select * from csc648.item', (err, res) => {
+        let arrayOfItems = [];
+        res.map((data) => {
+            for (key in data) {
+                if (key == "item_pic") {
+                    let val = data[key];
+                    let url = getPicURL(val);
+                    if(url != -1){
+                        data[key] = url;
+                    }                    
+                }
             }
+            let sData = JSON.parse(JSON.stringify(data));
+            arrayOfItems.push(sData);
+        })
+        console.log(arrayOfItems);
+        return arrayOfItems;
+    });
+}
+
+/*__________________________________________________________________________________________________*/
+
+//Use a parameter to find item with that word
+function getItem(searchWord) {
+    let searchw = "%" + searchWord + "%";
+    let baseSQL = 'select * from csc648.item where item_name like ? or item_desc like ?;';
+    pool.query(baseSQL, [searchw, searchw], (err, res) => {
+        let arrayOfItems = [];
+        if (res == undefined) {
+            //have to send something?
+
+            return -1;
         }
-        let sData = JSON.parse(JSON.stringify(data));       
-        arrayOfItems.push(sData);
-    })
-    console.log(arrayOfItems);
-    return arrayOfItems;
-}); */
+        res.map((data) => {
+            for (key in data) {
+                if (key == "item_pic") {
+                    let val = data[key];
+                    let url = getPicURL(val);
+                    if(url != -1){
+                        data[key] = url;
+                    }                    
+                }
+            }
+            let sData = JSON.parse(JSON.stringify(data));
+            arrayOfItems.push(sData);
+        })
+        console.log(arrayOfItems);
+        return arrayOfItems;
+    });
+}
 
+/*__________________________________________________________________________________________________*/
 
-//*****************
-//Actually searching
-let searchWord = "rare";
-
-let searchw = "%"+searchWord+"%";
-let baseSQL = 'select * from csc648.item where item_name like ? or item_desc like ?;';
-pool.query(baseSQL,[searchw,searchw], (err, res) => {
-    let arrayOfItems = [];
-   
-    if(res == undefined){
-        //have to send something?
-
+//Getting the URL of the image
+function getPicURL(name) {
+    try {
+        aws.config.setPromisesDependency();
+        aws.config.update({
+            accessKeyId: 'AKIA2JSQG5HH76ZUX4PE',
+            secretAccessKey: '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',
+            region: 'us-east-1'
+        });
+        const s3 = new aws.S3();
+        var params = {
+            Bucket: 'csc648-t8-user-uploaded-images',
+            Key: name
+        }
+        var url = s3.getSignedUrl('getObject', params);
+        //console.log(url);
+        return url;
+    } catch (e) {
+        console.log('AWS error response', e);
         return -1;
     }
-    res.map((data) => {
-        for(key in data){
-            if(key == "item_pic"){
-                let val = data[key];
-                //here run the s3 stuff to get the image url. find using the value and save result
-
-
-                //then replace the val with the saved url result
-                let x = "temp_url"
-                data[key] = x;
-            }
-        }
-        let sData = JSON.parse(JSON.stringify(data));       
-        arrayOfItems.push(sData);
-    })
-    console.log(arrayOfItems);
-    return arrayOfItems;
-});
-//___________________________________________________________
+}
