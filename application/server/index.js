afa5997ea59a0c5f96035a37ab9d28489722b4b1
Mark Kim <mark@MacMini.local.lan>
2022-03-21 13:35:30 -0700

    changed listening port for index.js

--- application/server/index.js
+++ application/server/index.js
@@ -105,5 +105,5 @@ app.get("/getpic/:name", (req, res) => {
   res.send({ url: url });
 });
 
-const port = 8080;
+const port = 3000 ;
 app.listen(port, () => console.log("App is listening on port ", port));
