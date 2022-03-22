7a7cb21c9be36e6f999f79ed4a65a70a8ec4f194
Mark Kim <mark@MacMini.local.lan>
2022-03-21 20:57:03 -0700

    changed baseurl to localhost

--- application/server/index.js
+++ application/server/index.js
@@ -114,7 +114,7 @@ app.get("/getpic/:name", (req, res) => {
 });
 
 const port = 8080;
-// app.listen(port, () => console.log("App is listening on port ", port));
-const server = https.createServer(options, app).listen(port, () => {
-  console.log("Express server listening on port " + port);
-})
+app.listen(port, () => console.log("App is listening on port ", port));
+// const server = https.createServer(options, app).listen(port, () => {
+//   console.log("Express server listening on port " + port);
+// })
