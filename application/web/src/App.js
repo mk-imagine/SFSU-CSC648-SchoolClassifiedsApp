527fd7c0e46863beca9e1addce452a10f4af0317
Khushboo Gandhi <khushboo1028@gmail.com>
2022-04-12 18:00:07 -0700

    login track added

--- application/web/src/App.js
+++ application/web/src/App.js
@@ -9,12 +9,9 @@ import Jesus from "../src/components/About/People/Jesus";
 import Jiasheng from "../src/components/About/People/Jiasheng";
 import Vivian from "../src/components/About/People/Vivian";
 import Mark from "../src/components/About/People/Mark";
-<<<<<<< HEAD
-import Login from "../src/components/Login";
-=======
 import About from "./Pages/About";
 import RegisterPage from "./Pages/Register";
->>>>>>> 5dfa5163be02cccb1a9b10686f62c2c2c5a301aa
+import LoginPage from "./Pages/LoginPage";
 
 function App() {
   return (
@@ -30,15 +27,11 @@ function App() {
         <Route path="/about/Jesus" exact element={<Jesus />} />
         <Route path="/about/Jiasheng" exact element={<Jiasheng />} />
         <Route path="/about/Vivian" exact element={<Vivian />} />
-<<<<<<< HEAD
-        <Route path="/login" exact element={<Login />}/>
-        {/* <Route path="*" component={NotFound} /> */}
-=======
 
         <Route path="/register" exact element={<RegisterPage />} />
+        <Route path="/login" exact element={<LoginPage />} />
 
         {/* <Route path="*" element={NotFound} /> */}
->>>>>>> 5dfa5163be02cccb1a9b10686f62c2c2c5a301aa
       </Routes>
     </Router>
   );
