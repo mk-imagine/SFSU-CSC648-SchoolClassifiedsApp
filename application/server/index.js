6153a0d1d98e289c4596d4c0ee249d4706feb113
Cody Huang <wenhuang@umail.ucsb.edu>
2022-04-13 16:28:09 -0700

    uncommented out sessions in index.js for testing. Moved session definition to before app.use

--- application/server/index.js
+++ application/server/index.js
@@ -6,11 +6,37 @@ const postingrouter = require('./routes/posting');
 const loginrouter = require('./routes/login');
 const registerrouter = require('./routes/register');
 
+var sessions = require('express-session');
+var mysqlSession = require('express-mysql-session')(sessions);
+
 const app = express();
 
-// var sessions = require('express-session');
-// var mysqlSession = require('express-mysql-session')(sessions);
+
 const flash = require("express-flash");
+//configure sessions
+var mysqlSessionStore = new mysqlSession(
+    {
+        /*using default options*/
+    },
+    require('./routes/database')
+);
+app.use(sessions({
+    key: "csid",//key used for cookie for front end
+    secret: "csc648",//used to sign cookie
+    store: mysqlSessionStore,
+    resave: false,
+    saveUninitialized: false//don't save sessions that we don't initialize ourselves
+}));
+//keep track of login/out state
+app.use((req, res, next) => {
+    console.log(req.session);
+    if(req.session.username) {
+        res.locals.logged = true;
+    }
+    next();
+})
+
+//app.use('/createpost', postingrouter);
 
 app.use('/api', dbrouter);
 
@@ -20,29 +46,7 @@ app.use('/login',loginrouter);
 app.use('/post', postingrouter);
 app.use('/register', registerrouter);
 
-//app.use('/createpost', postingrouter);
-// //configure sessions
-// var mysqlSessionStore = new mysqlSession(
-//     {
-//         /*using default options*/
-//     },
-//     require('./routes/database')
-// );
-// app.use(sessions({
-//     key: "csid",//key used for cookie for front end
-//     secret: "csc648",//used to sign cookie
-//     store: mysqlSessionStore,
-//     resave: false,
-//     saveUninitialized: false//don't save sessions that we don't initialize ourselves
-// }));
-// //keep track of login/out state
-// app.use((req, res, next) => {
-//     console.log(req.session);
-//     if(req.session.username) {
-//         res.locals.logged = true;
-//     }
-//     next();
-// })
+
 
 app.use(flash());
 
