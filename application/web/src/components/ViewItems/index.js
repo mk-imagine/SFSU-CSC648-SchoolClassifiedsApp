f3c4aa96b11a54af92ed37592c6efd23138f2c43
Mark Kim <mark@MacMini.local.lan>
2022-04-18 09:13:39 -0700

    Revert backend host back to localhost for development

--- application/web/src/components/ViewItems/index.js
+++ application/web/src/components/ViewItems/index.js
@@ -13,8 +13,8 @@ export const ViewItems = (props) => {
   const columnsPerRow = 3;
 
   const navigate = useNavigate();
-  const base_url = "/api";
-  // const base_url = "http://localhost:3100/api";
+  // const base_url = "/api";
+  const base_url = "http://localhost:3100/api";
   // eslint-disable-next-line
   useEffect(() => {
     setItems(props.items);
