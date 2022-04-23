86e14592b819a684e6b83ebf1c7835c50b0f14c6
Ernesto Diaz <ernestodiaz@Ernestos-MacBook-Pro.local>
2022-04-23 12:12:11 -0700

    bl

--- application/server/config/db2.js
+++ application/server/config/db2.js
@@ -6,7 +6,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: "localhost",
     user:"root",
-    password:"csc648team8",
+    password:"Gatorbear1994",
     database:"csc648",
     connectionLimit: 50,
     debug:false
