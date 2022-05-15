a5391170318d8474b7de70da0a4549349aff4a01
Mark Kim <mark@MacMini.local.lan>
2022-05-14 22:34:04 -0700

    added item_approved to inserts.sql so that we have approved items in test data

--- application/server/database/inserts.sql
+++ application/server/database/inserts.sql
@@ -25,30 +25,30 @@ VALUES
 ("mkim", "Mark", "Kim", "mkim22@mail.sfsu.edu", "student", "password");
 
 INSERT INTO item (item_name, item_desc, item_price, item_pic, item_category, item_seller_id, 
-item_thumbnail, item_created, item_golive_time, item_course, item_postexpires)
+item_thumbnail, item_created, item_golive_time, item_course, item_postexpires, item_approved)
 VALUES
 ("Rare painting", "Up and coming artist.  Will be priceless in 5 yrs!", 100.00, 'art.jpeg', 1, 1, 
-'thumbnail-art.jpeg', '2022-01-01 05:30:00', '2022-01-02 05:30:00', NULL, '2023-01-02 05:30:00'),
+'thumbnail-art.jpeg', '2022-01-01 05:30:00', '2022-01-02 05:30:00', NULL, '2023-01-02 05:30:00', 0),
 ("Dictionary", "It's a dictionary", 15.00, 'dictionary.jpeg', 2, 2, 
-'thumbnail-dictionary.jpeg', '2022-01-15 05:30:00', '2022-01-16 05:30:00', NULL, '2023-01-02 05:30:00'),
+'thumbnail-dictionary.jpeg', '2022-01-15 05:30:00', '2022-01-16 05:30:00', NULL, '2023-01-02 05:30:00', 0),
 ("Gooseneck Kettle", "For your everyday coffee.", 25.00, 'gooseneckkettle.jpeg', 6, 4, 
-'thumbnail-gooseneckkettle.jpeg', '2022-02-01 05:30:00', '2022-02-02 05:30:00', NULL, '2023-01-02 05:30:00'),
+'thumbnail-gooseneckkettle.jpeg', '2022-02-01 05:30:00', '2022-02-02 05:30:00', NULL, '2023-01-02 05:30:00', 0),
 ("Bicycle Helmet", "To protect your brain on dangerous SF roads.", 30.00, 'helmet.jpeg', 8, 3, 
-'thumbnail-helmet.jpeg', '2022-02-15 05:30:00', '2022-02-16 05:30:00', NULL, '2023-01-02 05:30:00'),
+'thumbnail-helmet.jpeg', '2022-02-15 05:30:00', '2022-02-16 05:30:00', NULL, '2023-01-02 05:30:00', 1),
 ("Ice Sculpture", "For incredible parties! Pick it up soon before it melts", 999.00, 'icesculpture.jpeg', 1, 7, 
-'thumbnail-icesculpture.jpeg', '2022-03-01 05:30:00', '2022-03-02 05:30:00', NULL, '2023-01-02 05:30:00'),
+'thumbnail-icesculpture.jpeg', '2022-03-01 05:30:00', '2022-03-02 05:30:00', NULL, '2023-01-02 05:30:00', 1),
 ("A white lamp", "So you can stay up late, studying.  It's boring so it won't distract you.", 50.00, 'lamp.jpeg', 5, 6, 
-'thumbnail-lamp.jpeg', '2022-03-15 05:30:00', '2022-03-16 05:30:00', NULL, '2023-01-02 05:30:00'),
+'thumbnail-lamp.jpeg', '2022-03-15 05:30:00', '2022-03-16 05:30:00', NULL, '2023-01-02 05:30:00', 1),
 ("Snowboard", "Jump on this popsicle stick and barrel down the mountain! WHEEE!", 200.00, 'snowboard.jpeg', 8, 5, 
-'thumbnail-snowboard.jpeg', '2022-04-01 05:30:00', '2022-04-02 05:30:00', NULL, '2023-01-02 05:30:00'),
+'thumbnail-snowboard.jpeg', '2022-04-01 05:30:00', '2022-04-02 05:30:00', NULL, '2023-01-02 05:30:00', 1),
 ("Stapler", "Channel your inner Office Space", 10.00, 'stapler.jpeg', 7, 3, 
-'thumbnail-stapler.jpeg', '2022-04-08 05:30:00', NULL, NULL, '2023-01-02 05:30:00'),
+'thumbnail-stapler.jpeg', '2022-04-08 05:30:00', NULL, NULL, '2023-01-02 05:30:00', 1),
 ("Constitution and the Nation: The Regulatory State", "A required book for HIST 471", 10.00, 'regstate.jpeg', 3, 8, 
-'thumbnail-regstate.jpeg', '2022-04-08 05:30:00', NULL, 'HIST 471', '2023-01-02 05:30:00'),
+'thumbnail-regstate.jpeg', '2022-04-08 05:30:00', NULL, 'HIST 471', '2023-01-02 05:30:00', 1),
 ("Constitution and the Nation: A Revolution in Rights", "A required book for HIST 471", 10.00, 'revrights.jpeg', 3, 8, 
-'thumbnail-revrights.jpeg', '2022-04-08 05:30:00', NULL, 'HIST 471', '2023-01-02 05:30:00'),
+'thumbnail-revrights.jpeg', '2022-04-08 05:30:00', NULL, 'HIST 471', '2023-01-02 05:30:00', 1),
 ("Brown v. Board of Education", "A required book for HIST 471", 10.00, 'bvb.jpeg', 3, 8, 
-'thumbnail-bvb.jpeg', '2022-04-08 05:30:00', NULL, 'HIST 471', '2023-01-02 05:30:00');
+'thumbnail-bvb.jpeg', '2022-04-08 05:30:00', NULL, 'HIST 471', '2023-01-02 05:30:00', 1);
 
 INSERT INTO message (msg_sender, msg_recipient, msg_meet_time, msg_location, msg_contactinfo, msg_body, msg_timestamp)
 VALUES
