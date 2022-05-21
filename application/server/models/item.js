90a0369d2bb4aa65e62833bdb57061afc2306d6f
Mark Kim <mark@MacMini.local.lan>
2022-05-21 11:58:28 -0700

    fixed item model functions to mirror getallitems function to include table joins

--- application/server/models/item.js
+++ application/server/models/item.js
@@ -65,16 +65,24 @@ ItemsModel.getAllItems = (order, direction) => {
 ItemsModel.categoryAndItemSearch = (category, searchWord, order, direction) => {
     let baseSQL;
     if(order == 'price'){
-        baseSQL = `select * from csc648.item
-        left join csc648.category on item.item_category = category.category_id
-        where (item.item_name like ? or item.item_desc like ?) and category.category_name like ? and item_approved = 1
+        baseSQL = `SELECT it.item_id, it.item_category, cat.category_name, it.item_name, it.item_desc, it.item_price, it.item_pic, 
+        it.item_thumbnail, it.item_created, it.item_course, it.item_postexpires, seller.user_username,
+        seller.user_fname, seller.user_lname, seller.user_id AS "sellerid", it.item_approved AS "itemapproved"
+        FROM csc648.item it
+        INNER JOIN user seller ON seller.user_id = it.item_seller_id 
+        INNER JOIN category cat ON cat.category_id = it.item_category
+        where (it.item_name like ? or it.item_desc like ?) and cat.category_name like ? and item_approved = 1
         order by item_price ${direction};`;
     }
     if(order == 'date'){
-        baseSQL = `select * from csc648.item
-                    left join csc648.category on item.item_category = category.category_id
-                    where (item.item_name like ? or item.item_desc like ?) and category.category_name like ? and item_approved = 1
-                    order by item_created ${direction}`;
+        baseSQL = `SELECT it.item_id, it.item_category, cat.category_name, it.item_name, it.item_desc, it.item_price, it.item_pic, 
+        it.item_thumbnail, it.item_created, it.item_course, it.item_postexpires, seller.user_username,
+        seller.user_fname, seller.user_lname, seller.user_id AS "sellerid", it.item_approved AS "itemapproved"
+        FROM csc648.item it
+        INNER JOIN user seller ON seller.user_id = it.item_seller_id 
+        INNER JOIN category cat ON cat.category_id = it.item_category
+        where (it.item_name like ? or it.item_desc like ?) and cat.category_name like ? and item_approved = 1
+        order by item_created ${direction};`;
     }
     return db.execute(baseSQL, [searchWord, searchWord, category])
         .then(([results, fields]) => {
@@ -90,10 +98,14 @@ ItemsModel.categoryAndItemSearch = (category, searchWord, order, direction) => {
  */
 ItemsModel.categorySearch = (category, orderby, direction) => {
     if( orderby == "price") {
-        let baseSQL = `select * from csc648.item
-                        left join csc648.category on item.item_category = category.category_id
-                        WHERE category.category_name like ? and item.item_approved = 1
-                        ORDER BY item_price ${direction};`;
+        let baseSQL = `SELECT it.item_id, it.item_category, cat.category_name, it.item_name, it.item_desc, it.item_price, it.item_pic, 
+        it.item_thumbnail, it.item_created, it.item_course, it.item_postexpires, seller.user_username,
+        seller.user_fname, seller.user_lname, seller.user_id AS "sellerid", it.item_approved AS "itemapproved"
+        FROM csc648.item it
+        INNER JOIN user seller ON seller.user_id = it.item_seller_id 
+        INNER JOIN category cat ON cat.category_id = it.item_category
+        WHERE cat.category_name like ? and it.item_approved = 1
+        ORDER BY item_price ${direction};`;
         return db.execute(baseSQL, [category])
         .then(([results, fields]) => {
         return Promise.resolve(results);
@@ -101,10 +113,14 @@ ItemsModel.categorySearch = (category, orderby, direction) => {
         .catch((err) => Promise.reject(err));
     }
     if(orderby == "date") {
-        let baseSQL = `select * from csc648.item
-                        left join csc648.category on item.item_category = category.category_id
-                        WHERE category.category_name like ? and item.item_approved = 1
-                        ORDER BY item_created ${direction};`;
+        let baseSQL = `SELECT it.item_id, it.item_category, cat.category_name, it.item_name, it.item_desc, it.item_price, it.item_pic, 
+        it.item_thumbnail, it.item_created, it.item_course, it.item_postexpires, seller.user_username,
+        seller.user_fname, seller.user_lname, seller.user_id AS "sellerid", it.item_approved AS "itemapproved"
+        FROM csc648.item it
+        INNER JOIN user seller ON seller.user_id = it.item_seller_id 
+        INNER JOIN category cat ON cat.category_id = it.item_category
+        WHERE cat.category_name like ? and it.item_approved = 1
+        ORDER BY item_created ${direction};`;
         return db.execute(baseSQL, [category])
         .then(([results, fields]) => {
         return Promise.resolve(results);
@@ -123,13 +139,23 @@ ItemsModel.categorySearch = (category, orderby, direction) => {
 ItemsModel.itemSearch = (searchWord, orderby, direction) => {
     let baseSQL;
     if (orderby == "price") {
-        baseSQL = `select * from csc648.item
-        where (item_name like ? or item_desc like ?) AND item_approved = 1
+        baseSQL = `SELECT it.item_id, it.item_category, cat.category_name, it.item_name, it.item_desc, it.item_price, it.item_pic, 
+        it.item_thumbnail, it.item_created, it.item_course, it.item_postexpires, seller.user_username,
+        seller.user_fname, seller.user_lname, seller.user_id AS "sellerid", it.item_approved AS "itemapproved"
+        FROM csc648.item it
+        INNER JOIN user seller ON seller.user_id = it.item_seller_id 
+        INNER JOIN category cat ON cat.category_id = it.item_category
+        where (it.item_name like ? or it.item_desc like ?) AND item_approved = 1
         ORDER BY item_price ${direction};`;
     }
     if (orderby == "date") {
-        baseSQL = `select * from csc648.item
-        where (item_name like ? or item_desc like ?) AND item_approved = 1
+        baseSQL = `SELECT it.item_id, it.item_category, cat.category_name, it.item_name, it.item_desc, it.item_price, it.item_pic, 
+        it.item_thumbnail, it.item_created, it.item_course, it.item_postexpires, seller.user_username,
+        seller.user_fname, seller.user_lname, seller.user_id AS "sellerid", it.item_approved AS "itemapproved"
+        FROM csc648.item it
+        INNER JOIN user seller ON seller.user_id = it.item_seller_id 
+        INNER JOIN category cat ON cat.category_id = it.item_category
+        where (it.item_name like ? or it.item_desc like ?) AND item_approved = 1
         ORDER BY item_created ${direction};`;       
     }
     return db.execute(baseSQL, [searchWord, searchWord])
